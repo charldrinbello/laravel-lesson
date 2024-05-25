e43726be4c1d9bfb534e94ca1a9671b64370aa63
charldrinbello <charldrinbello@gmail.com>
2024-05-25 20:17:09 +0800

    dashboard has been fix successfully

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,11 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; Copyright <strong><span>NiceAdmin</span></strong
+        &copy; Copyright <strong><span>Laravel Lesson</span></strong
         >. All Rights Reserved
     </div>
-
+    <div class="credits">
+        
+        Modofied by: <a href="https://bootstrapmade.com/">Charldrin Bello</a>
+    </div>
 </footer>
+<!-- End Footer -->
